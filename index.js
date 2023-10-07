155a8f65bbc6dcf9e774a85e2b0380b0a0bb7e16
Daniel Mutuku <danielmakau507@gmail.com>
2023-10-07 15:17:25 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n*2) this is because we are iterating in a nested loop
 */
 
 /* 
   Add your pseudocode here
+  -We use two nested loops to iterate through each pair of elements in the array.
+  -For each pair, we check if their sum equals the target.
+  -If we find a pair that satisfies the condition, we return true.
+  -If no such pair is found after checking all pairs, we return false.
 */
 
 /*
   Add written explanation of your solution here
+  You are given an array of integers (array) and an integer (target). Write a function hasTargetSum that returns true if there are two numbers in the array that add up to the target, and false otherwise.
 */
 
 // You can run `node index.js` to view these console logs
